3380192320543848a05c85b4051d8655e2ae949e
Priz Tamiano <priz.candy@gmail.com>
2019-10-12 12:43:13 -0300

    commit archivos modificados

---
+++ FINAL/script.js
@@ -0,0 +1,17 @@
+const HOME = document.getElementById('homeSection');
+const HISTORY = document.getElementById('nuestraHistoria');
+const PRODUCTS = document.getElementById('productosSection');
+const RUMP = document.getElementById('rump');
+const INTI = document.getElementById('inti');
+const CONTACT = document.getElementById('contactSection');
+
+const HOMELINK = document.getElementById('homeLi');
+const HISTORYLINK = document.getElementById('historyLi');
+const PRODUCTSLINK = document.getElementById('productLi');
+const CERTLINK = document.getElementById('certLi');
+const RUMPLINK = document.getElementById('rumpLi');
+const INTILINK = document.getElementById('intiLi');
+const CONTACTLINK = document.getElementById('contactLi');
+
+
+
